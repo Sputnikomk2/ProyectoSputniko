2df3a68cc98db60e17570de193cad01e6f754ff3
Carmenalbl <158594581+Carmenalbl@users.noreply.github.com>
2024-02-27 17:53:59 +0100

    He cambiado la clase Aprendizaje

--- doc/Aprendizaje.py
+++ doc/Aprendizaje.py
@@ -2,14 +2,14 @@ class Aprendizaje:
     def __init__(self, nivel1):
         self.nivel = nivel1
 
-    def obtener_nivel1(self):
+    def obtener_aprendizaje_nivel1(self):
         return self.nivel1
 
     
     def __init__(self,nivel2):
         self.nivel2=nivel2
 
-    def obtener_nivel2(self):
+    def obtener_aprendizaje_nivel2(self):
         return self.nivel2
 
 
