0c6f33e47c9adf7ab2cbb7b27a38ec14841333f5
Carmenalbl <158594581+Carmenalbl@users.noreply.github.com>
2024-02-27 17:46:22 +0100

    he_modificado_clase_aprendizaje

---
+++ doc/Aprendizaje.py
@@ -0,0 +1,17 @@
+class Aprendizaje:
+    def __init__(self, nivel1):
+        self.nivel = nivel1
+
+    def obtener_nivel1(self):
+        return self.nivel1
+
+    
+    def __init__(self,nivel2):
+        self.nivel2=nivel2
+
+    def obtener_nivel2(self):
+        return self.nivel2
+
+
+
+
