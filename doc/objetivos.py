24984d18035e5d42ee3c30f1bafbaba71305cc0c
Carmenalbl <158594581+Carmenalbl@users.noreply.github.com>
2024-03-04 19:45:57 +0100

    He cambiado el codigo

--- doc/objetivos.py
+++ doc/objetivos.py
@@ -1,10 +1,7 @@
 
 # Definir la constante
-MEDIO_AMBIENTE = {
-
-    "motivacion": "Mantener la motivación del alumnado sobre el proceso de aprendizaje",
-    "aprendizaje": "Fomentar el conocimiento sobre los ecosistemas, la biodiversidad y las prácticas sostenibles"
-}
+from enum import Enum
+objetivos = Enum('objetivos' , [ 'aprendizaje_ma', 'motivacion'])
 
 
 
---
+++ juegoSputniko/objetivos.py
@@ -0,0 +1,19 @@
+
+# Definir la constante
+MEDIO_AMBIENTE = {
+
+    "motivacion": "Mantener la motivación del alumnado sobre el proceso de aprendizaje",
+    "aprendizaje": "Fomentar el conocimiento sobre los ecosistemas, la biodiversidad y las prácticas sostenibles"
+}
+
+
+
+
+
+
+
+
+
+
+
+
