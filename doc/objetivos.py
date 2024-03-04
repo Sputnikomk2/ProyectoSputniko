96478322fc6f0bd39c5fa93a1735b537c0cdc8a4
Carmenalbl <158594581+Carmenalbl@users.noreply.github.com>
2024-03-04 19:39:46 +0100

    close #40

--- doc/objetivos.py
+++ doc/objetivos.py
@@ -3,7 +3,7 @@
 MEDIO_AMBIENTE = {
 
     "motivacion": "Mantener la motivación del alumnado sobre el proceso de aprendizaje",
-    "aprendizaje": "Fomentar el conocimiento sobre los ecosistemas, la biodiversidad y las prácticas sostenibles."
+    "aprendizaje": "Fomentar el conocimiento sobre los ecosistemas, la biodiversidad y las prácticas sostenibles"
 }
 
 
