c998c539ebd0a8bf7b574b916be93f3461d42845
Carmenalbl <158594581+Carmenalbl@users.noreply.github.com>
2024-03-06 19:14:07 +0100

    Añadido el archivo partida #64

---
+++ sputnikgame/juego.py
@@ -0,0 +1,5 @@
+class Juego:
+    def __init__(self, idalumno, objetivos=None, puntuacion=0):
+        self.id = idalumno
+        self.objetivos = objetivos if objetivos else []
+        self.puntuacion = puntuacion
