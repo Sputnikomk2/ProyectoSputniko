2b0edf47cedc8b51f83cd6cea6ae4553523207f4
Carlos ALvarez <carlosmail1978@gmail.com>
2024-03-07 20:32:15 +0100

    Cambia nombre archivos y elimina archivo ddd.py

--- sputnikgame/ddd.py
+++
@@ -1,9 +0,0 @@
-class Juego:
-    def __init__(self, idalumno, objetivos=None, puntuacion=0):
-        self.id = idalumno
-        self.objetivos = objetivos if objetivos else []
-        self.puntuacion = puntuacion
-
-
-
-
