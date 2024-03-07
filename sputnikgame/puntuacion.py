cba8fa91d9bdb06453ba08e5d95cdb5f42f1c70a
Carlos ALvarez <carlosmail1978@gmail.com>
2024-03-07 19:21:28 +0100

    Añade archivo puntuacion issue #41

--- sputnikgame/puntuacion.py
+++ sputnikgame/puntuacion.py
@@ -4,9 +4,9 @@ class Puntuacion:
         if puntos == 500:
             return "1 manzana"
         elif puntos == 2500:
-            return "1 colacao ó 1 tostada"
+            return "1 café ó 1 tostada"
         elif puntos == 5000:
-            return "1 manzana, 1 colacao, 1 tostada"
+            return "1 manzana, 1 café, 1 tostada"
         else:
             return "puntos no válidos"
 
