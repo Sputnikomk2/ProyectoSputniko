7555a0dbd8696f42ffc664b6de78e6b216661e75
Francisco Javier Peña Jiménez <franciscojavierpenajimenez@gmail.com>
2024-03-07 17:37:10 +0100

    Elimina atributo id de estudiante #68

--- sputnikgame/alumno.py
+++ sputnikgame/alumno.py
@@ -1,6 +1,5 @@
-class Alumno:
+class Estudiante:
     
-    def __init__(self, id: int, alumno_nickname: str):
-        self.id = id
+    def __init__(self, alumno_nickname: str):
         self.alumno_nickname = alumno_nickname
 
