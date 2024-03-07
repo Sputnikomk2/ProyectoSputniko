2d34ee7495cc1bd320ab0d9307ed40c442d146e2
Carlos ALvarez <carlosmail1978@gmail.com>
2024-03-07 18:00:19 +0100

    Modifica archivo puntuacion.py issue #41 eliminado por error y actualiza nombre de la clase Estudiante por errata

--- sputnikgame/puntuacion.py
+++ sputnikgame/puntuacion.py
@@ -1,3 +1,14 @@
-# Definir la constante
-from enum import Enum
-objetivos = Enum('objetivos', ['aprendizaje_medio_ambiente', 'motivacion_durante_el_aprendizaje'])
+def generar_premio(puntos):
+    if puntos == 500:
+        return "1 manzana"
+    elif puntos == 2500:
+        return "1 colacao ó 1 tostada"
+    elif puntos == 5000:
+        return "1 manzana, 1 colacao, 1 tostada"
+    else:
+        return "puntos no válidos"
+
+
+puntos = 5000
+premio = generar_premio(puntos)
+print(premio)
